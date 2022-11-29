035da6a278da1888b3b4453e3fba555a00e78742
Anton <71523607+AnToxa566@users.noreply.github.com>
2022-11-29 13:19:58 +0200

    feat: Added new models - Lecture and Comment
     - Created two new models and their migrations - "Lecture" and "Comment". These models were binded among themselves, as well as with conferences and users. And also migrated to the database.

---
+++ app/Models/Lecture.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lecture extends Model
+{
+    use HasFactory;
+
+    protected $table = 'lectures';
+
+    protected $fillable = [
+        'title',
+        'date_time_start',
+        'date_time_end',
+        'description',
+        'presentation',
+    ];
+
+    public function user()
+    {
+      return $this->belongsTo(User::class);
+    }
+
+    public function conference()
+    {
+      return $this->belongsTo(Conference::class);
+    }
+
+    public function comments()
+    {
+      return $this->hasMany(Comment::class);
+    }
+}
