87a8166936d030fe7733b53be3d1ad3222bac455
Anton <71523607+AnToxa566@users.noreply.github.com>
2023-02-09 15:30:39 +0200

    feat: Added Lecture Store by Admin Tests

--- tests/Feature/LectureStoreByAdminTest.php
+++ tests/Feature/LectureStoreByAdminTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
+use Illuminate\Http\UploadedFile;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -28,20 +29,84 @@ class LectureStoreByAdminTest extends TestCase
     }
 
 
-    // public function testSuccessfulStoreLecture(): void
-    // {
-    //     // dd($this->getLectureDataToStore());
+    protected function testNotAdminTryingToStoreLecture(string $userType): void
+    {
+        $lectureData = $this->getLectureDataToStore();
 
-    //     $response = $this
-    //         ->actingAs(User::factory()->admin()->create())
-    //         ->postJson(
-    //             '/nova-api/lectures/',
-    //             $this->getLectureDataToStore()
-    //         )
-    //         ->assertSuccessful();
+        $this
+            ->actingAs(User::factory()->{$userType}()->create())
+            ->postJson(
+                '/nova-api/lectures/',
+                $lectureData
+            )
+            ->assertForbidden();
 
-    //     $this->assertDatabaseHas('lectures', [
-    //         'id' => $response['id'],
-    //     ]);
-    // }
+        $this->assertDatabaseMissing('lectures', [
+            'title' => $lectureData['title'],
+        ]);
+    }
+
+
+    public function testSuccessfulStoreLecture(): void
+    {
+        $response = $this
+            ->actingAs(User::factory()->admin()->create())
+            ->postJson(
+                '/nova-api/lectures/',
+                $this->getLectureDataToStore()
+            )
+            ->assertSuccessful();
+
+        $this->assertDatabaseHas('lectures', [
+            'id' => $response['id'],
+        ]);
+    }
+
+    public function testListenerTryingToStoreLecture(): void
+    {
+        $this->testNotAdminTryingToStoreLecture('listener');
+    }
+
+
+    public function testAnnouncerTryingToStoreLecture(): void
+    {
+        $this->testNotAdminTryingToStoreLecture('announcer');
+    }
+
+
+    public function testUnauthorizedTryingToStoreLecture(): void
+    {
+        $lectureData = $this->getLectureDataToStore();
+
+        $this
+            ->postJson(
+                '/nova-api/lectures/',
+                $lectureData
+            )
+            ->assertUnauthorized();
+
+        $this->assertDatabaseMissing('lectures', [
+            'title' => $lectureData['title'],
+        ]);
+    }
+
+
+    public function testStoreLectureWithInvalidData(): void
+    {
+        $lectureData = $this->getLectureDataToStore();
+        $lectureData['title'] = '';
+
+        $this
+            ->actingAs(User::factory()->admin()->create())
+            ->postJson(
+                '/nova-api/lectures/',
+                $lectureData
+            )
+            ->assertUnprocessable()
+            ->assertJsonValidationErrors('title');
+
+        $this->assertDatabaseMissing('lectures', [
+            'title' =>  $lectureData['title'],
+        ]);
+    }
 }
